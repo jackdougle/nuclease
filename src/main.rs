5db06de731932625ff61ab11cf20f35b4f815215
Jack Douglass <jackdouglass@JackBook-Pro.local>
2025-10-05 08:42:58 -0700

    fixed documentation formatting

--- src/main.rs
+++ src/main.rs
@@ -8,24 +8,26 @@ const ABOUT: &str = "Nuclease 1.0.3
 Written by Jack Douglass
 Last modified October 5, 2025
 
-Purpose: Compares DNA sequences from input file to DNA sequences from reference
+Nuclease compares DNA sequences from input file to DNA sequences from reference
  file using k-mer analysis. Splits up reference file sequences into k-mers of 
  specified length to build k-mer index, then compares each input read sequence 
  for matching k-mers. If a read sequence has >= minhits matching k-mers, it 
  will be printed as a match. Very memory-efficient and performant. Processes 
  paired reads in two files or as a single interleaved file.
 
+USAGE: nuclease --in <reads file> --ref <ref file> --outm <file> --outu <file>
+
 INPUT PARAMETERS
     --in <file>         Input FASTA/FASTQ file containing reads to be filtered.
                         Use 'stdin.fq' or 'stdin' to pipe from stdin.
     --in2 <file>        (Optional) Second input file for 2nd pair of reads. 
                         Must be same length as main input file.
-    --ref <file>        Reference FASTA/FASTQ file containing sequences to 
+    --ref <file>        (-r) Reference FASTA/FASTQ file containing sequences to 
                         build reference k-mer index. Program will serialize
                         reference k-mers and build to --binref path for future
                         use. Not necessary if '--binref <file>' is provided.
-    --binref <file>     (Optional) Binary file containing serialized k-mer 
-                        index, increases performance. Nuclease makes this
+    --binref <file>     (-b) Binary file containing serialized k-mer index,
+                        increases performance. Nuclease makes this
                         automatically based on '--ref' file if path is also
                         given here. Increases speed.
 
@@ -36,22 +38,24 @@ OUTPUT PARAMETERS: use 'stdout.fa / stdout.fq to pipe to stdout'
     --outu <file>       Output file for reads that have < minhits k-mer matches
                         to reference k-mers. FASTA format if .fa, .fna, or
                         .fasta, FASTQ format otherwise.
-    --outm2 <file>      (Optional) Output file for 2nd pair of matched reads.
-    --outu2 <file>      (Optional) Output file for 2nd pair of unmatched reads.
+    --outm2 <file>      Output file for 2nd pair of matched reads.
+    --outu2 <file>      Output file for 2nd pair of unmatched reads.
 
 MEMORY & PERFORMANCE PARAMETERS
-    --k 21              K-mer size (number of bases per k-mer). Ranges from 
-                        1-31, larger k-mers will have less matches.
-    --minhits 1         Minimum number of k-mer matches a read sequence must
-                        have to be considered a match.
-    --threads auto      Number of threads to use for parallel processing.
-    --maxmem auto       Maximum memory to use. Program will use ~50% of
+    --k 21              (-k) K-mer size (number of bases per k-mer). Ranges
+                        from 1-31, larger k-mers will have less matches.
+    --minhits 1         (-h) Minimum number of k-mer matches a read sequence
+                        must have to be considered a match.
+    -t --threads auto   (-t) Number of threads to use for parallel processing.
+                        Program will use all available threads by default.
+    --maxmem auto       (-m) Maximum memory to use. Program will use ~50% of
                         available memory by default. '--maxmem 5G' will specify
                         5 gigabytes, '--maxmem 200M' will specify 200
                         megabytes. Memory limiting is currently a WIP.
-    --interinput        Enable flag to input as interleaved paired-end reads,
-                        omit flag for unpaired reads.
-    --order             Enable flag to get read outputs ordered by sequence ID.
+    --interinput        (-i) Enable flag to input as interleaved paired-end
+                        reads, omit flag for unpaired reads.
+    --order             (-o) Enable flag to get read outputs ordered by
+                        sequence ID.
 
 Function and usage documentation at ./README.md
 Contact jack.gdouglass@gmail.com for any questions or issues encountered.
@@ -73,11 +77,11 @@ struct Args {
     threads: Option<usize>,
 
     /// Min number of k-mer hits to match a read
-    #[arg(long)]
+    #[arg(short = 'h', long)]
     minhits: Option<u8>,
 
     /// Memory cap in human-readable format
-    #[arg(long)]
+    #[arg(short, long)]
     maxmem: Option<String>,
 
     /// FASTA/FASTQ path for reference sequences
